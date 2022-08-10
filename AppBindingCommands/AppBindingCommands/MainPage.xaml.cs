eb30987c71cf529b53d34145f15e2e9cb7bb4dfe
aamekst <mieko.alana@gmail.com>
2022-08-09 22:39:52 -0300

    [Commit initial]

---
+++ AppBindingCommands/AppBindingCommands/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppBindingCommands
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainPageViewModel();   
+
+        }
+
+        private void btnAtualizarInfo_Clicked(object sender, EventArgs e)
+        {
+            string informações = string.Empty;
+
+            if (Application.Current.Properties.ContainsKey("AcaoInicial"))
+                informações += Application.Current.Properties["AcaoInicial"];
+
+            if(Application.Current.Properties.ContainsKey("AcaoStart"))
+                informações += Application.Current.Properties["AcaoStart"];
+
+            if(Application.Current.Properties.ContainsKey("AcaoSleep"))
+                informações += Application.Current.Properties["AcaoSleep"];
+
+            if(Application.Current.Properties.ContainsKey("AcaoResume"))
+                informações += Application.Current.Properties["AcaoResume"];
+
+
+            lblInformacoes.Text = informações;
+
+
+
+        }
+    }
+}
---
+++ AppBindingCommands/AppBindingCommands/MainPageViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppBindingCommands
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void ShowMessage()
+        {
+            string dataProperty = Application.Current.Properties["dtAtual"].ToString();
+            DisplayMessage = $"Boa noiye {Name}. Hoje é {dataProperty}.";
+        }
+
+        public MainPageViewModel()
+        {
+            ShowMessageCommand = new Command(ShowMessage);
+        }
+
+
+
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public ICommand ShowMessageCommand;
+
+
+
+        private string name = string.Empty;
+
+        public string Name
+        {
+            get => name;//=> é return = {return name;}
+            set
+            {
+                if (name == null)
+                    return;
+
+                name = value;
+                OnPropertyChanged(nameof(Name));
+                OnPropertyChanged(nameof(DisplayName));
+
+            }
+
+        }
+
+        public string DisplayName => $"Nome digitado:  {Name}";
+
+        string displayMessage = string.Empty;
+        public string DisplayMessage
+        {
+            get => displayMessage;
+            set
+            {
+                if (displayMessage == null)
+                    return;
+
+                displayMessage = value;
+                OnPropertyChanged(nameof(DisplayMessage));
+            }
+        }
+
+
+    }
+}
